a14396b3a097376b9f2aab47107359c44d254c0a
Boris Demeshev <boris.demeshev@gmail.com>
2019-03-03 13:21:43 +0300

    update test

--- 2019-03-03/testing_new_watchdog.R
+++ 2019-03-03/testing_new_watchdog.R
@@ -15,8 +15,15 @@ watchdog = import(watchdog_file)
 # this was done to setup download log file for the first time
 # download_log_first = watchdog %>% mutate(access_date = Sys.Date(), access_status = NA, hash_raw = NA, hash_main = NA) %>% head(0)
 # export(download_log_first, download_log_file)
-download_log = import(download_log_file)
 
 
 
 download_log_new = download_statistics(path, watchdog)
+
+# first time:
+# export(download_log_new, download_log_file)
+
+# second and more:
+download_log = import(download_log_file)
+download_log = bind_rows(download_log, download_log_new)
+export(download_log, download_log_file)
