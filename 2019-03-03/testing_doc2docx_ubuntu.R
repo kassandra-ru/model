7ee28ce05d580c0b27c0fc0191548dc59648ed59
Boris Demeshev <boris.demeshev@gmail.com>
2019-03-05 10:50:21 +0300

    testing libreoffice

---
+++ 2019-03-03/testing_doc2docx_ubuntu.R
@@ -0,0 +1,16 @@
+lo_path = "/usr/bin/libreoffice"
+docx_dir = "~/Downloads/"
+doc_file = "~/Documents/kassandra/data/raw/2019-03-05/1-03.doc"
+
+add_on = "-env:LD_LIBRARY_PATH=:/usr/lib/libreoffice/program:/usr/lib/x86_64-linux-gnu/"
+
+cmd <- sprintf("\"%s\" --convert-to docx:\"MS Word 2007 XML\" --headless --outdir \"%s\" \"%s\"", 
+               lo_path, docx_dir, doc_file)
+cat(cmd)
+system(cmd, intern = TRUE)
+
+
+library(docxtractr)
+set_libreoffice_path("/usr/bin/libreoffice")
+
+tbl = docxtractr::read_docx("~/Downloads/1-11.doc")
--- 2019-03-03/testing_new_watchdog.R
+++ 2019-03-03/testing_new_watchdog.R
@@ -3,6 +3,13 @@ devtools::install_github("kassandra-ru/kassandr")
 library(kassandr)
 library(tidyverse)
 library(rio)
+library(lubridate)
+library(docxtractr)
+
+set_libreoffice_path("/usr/bin/libreoffice")  # ubuntu
+set_libreoffice_path("/usr/bin/libreoffice")  # macos
+set_libreoffice_path("C:/Program Files/LibreOffice/program/soffice.exe")  # windows
+
 
 path = "~/Documents/kassandra/data/raw/"
 
@@ -24,6 +31,6 @@ download_log_new = download_statistics(path, watchdog)
 # export(download_log_new, download_log_file)
 
 # second and more:
-download_log = import(download_log_file)
+download_log = import(download_log_file) %>% mutate(access_date = ymd(access_date))
 download_log = bind_rows(download_log, download_log_new)
 export(download_log, download_log_file)
