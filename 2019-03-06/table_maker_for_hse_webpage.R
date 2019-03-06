fc1dd54a7ee8b4285975f255790f7031d0af8dee
Boris Demeshev <boris.demeshev@gmail.com>
2019-03-06 08:31:43 +0300

    web tables

--- 2019-03-06/table_maker_for_hse_webpage.R
+++ 2019-03-06/table_maker_for_hse_webpage.R
@@ -3,7 +3,7 @@ library(tidyr)
 library(rio)
 library(kassandr)
 
-mae_table = import("mae_table_cpi.csv")
+mae_table = import("estimation_results/mae_table_cpi.csv")
 mae_post = mutate(mae_table, h = case_when(h == 1 ~ "1 месяц", 
                                            h == 2 ~ "2 месяца",
                                            h == 3 ~ "3 месяца",
@@ -12,21 +12,21 @@ mae_post = mutate(mae_table, h = case_when(h == 1 ~ "1 месяц",
                                            h == 6 ~ "6 месяцев"), mae = round(mae, 2))
 
 mae_wide = spread(mae_post, key = model_fun, value = mae)
-writexl::write_xlsx(mae_wide, "mae_cpi_wide.xlsx")
+writexl::write_xlsx(mae_wide, "web_tables/mae_cpi_wide.xlsx")
 
 
-forecast_table = import("forecasts_cpi.csv")
+forecast_table = import("estimation_results/forecasts_cpi.csv")
 forecast_post = mutate(forecast_table, point_forecast = round(point_forecast, 2))%>%
 mutate(date = date_to_string(date, freq = 12))
 
 forecast_wide = spread(forecast_post, key = model_fun, value = point_forecast) %>% arrange(h) %>% select(-h)
-writexl::write_xlsx(forecast_wide, "forecast_cpi_wide.xlsx")
+writexl::write_xlsx(forecast_wide, "web_tables/forecast_cpi_wide.xlsx")
 
 
 # gdp
 
 
-mae_table = import("mae_table_gdp_rate_real.csv")
+mae_table = import("estimation_results/mae_table_gdp_rate_real.csv")
 mae_post = mutate(mae_table, h = case_when(h == 1 ~ "1 квартал", 
                                            h == 2 ~ "2 квартала",
                                            h == 3 ~ "3 квартала",
@@ -34,15 +34,15 @@ mae_post = mutate(mae_table, h = case_when(h == 1 ~ "1 квартал",
                                            h == 5 ~ "5 кварталов",
                                            h == 6 ~ "6 кварталов"), mae = round(mae, 4))
 mae_wide = spread(mae_post, key = model_fun, value = mae)
-writexl::write_xlsx(mae_wide, "mae_gdp_wide.xlsx")
+writexl::write_xlsx(mae_wide, "web_tables/mae_gdp_wide.xlsx")
 
 
-forecast_table = import("forecasts_gdp_rate_real.csv")
+forecast_table = import("estimation_results/forecasts_gdp_rate_real.csv")
 forecast_post = mutate(forecast_table, point_forecast = round(point_forecast, 3)) %>% 
   mutate(date = date_to_string(date, freq = 4))
 
 forecast_wide = spread(forecast_post, key = model_fun, value = point_forecast) %>% arrange(h) %>% select(-h)
-writexl::write_xlsx(forecast_wide, "forecast_gdp_wide.xlsx")
+writexl::write_xlsx(forecast_wide, "web_tables/forecast_gdp_wide.xlsx")
 
 
 
