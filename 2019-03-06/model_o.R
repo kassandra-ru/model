60f5fbeeb25eddff3131775254fdf608d7481639
Oxana Malakhovskaya <oxana.malakhovskaya@gmail.com>
2019-03-06 10:57:47 +0300

    model_o (with ind_prod)

--- 2019-03-06/model_o.R
+++ 2019-03-06/model_o.R
@@ -152,7 +152,76 @@ only_numbers = select(the_forecasts_new, date, h, model_fun, point_forecast)
 only_numbers
 write_csv(only_numbers, path = "estimation_results/forecasts_gdp_rate_real.csv")
 
+# industrial product univariate models -------------------------------------------------------
 
+start_date = ymd("2013-01-01")
+
+ind_prod = import("data_snapshot/ind_okved2.csv")
+ind_prod_tsibble = mutate(ind_prod, date = yearmonth(date)) %>% as_tsibble(index = date)%>%rename(value = ind_prod)
+rus_m_full_stable = filter(ind_prod_tsibble, date >= start_date)
+
+
+rus_m_full_stable %>% tail()
+rus_m_full_stable %>% head()
+
+# industrial product quality evaluation --------------------------------------------------
+
+# TODO: rename test to eval
+
+proportion_test = 0.2 # доля ряда, используемая для оценки качества прогнозов
+
+nobs_full = nrow(rus_m_full_stable)
+nobs_test = round(proportion_test * nobs_full)
+
+window_type = "sliding" # "sliding" or "stretching" as in tsibble
+
+
+dates_test = tail(rus_m_full_stable$date, nobs_test)
+
+
+
+h_all = 1:6
+
+
+# model should take: h, model_sample (multivariate tsibble)
+# produce some model :)
+# may ignore h
+
+
+model_fun_tibble = tribble(~model_fun, ~h_agnostic, ~forecast_extractor, 
+                           "ets_fun", TRUE, "uni_model_2_scalar_forecast", 
+                           "tbats_fun", TRUE, "uni_model_2_scalar_forecast",
+                           "arima_fun", TRUE, "uni_model_2_scalar_forecast",
+                           "arima11_fun", TRUE, "uni_model_2_scalar_forecast")
+#                           "lasso_fun", FALSE, "lasso_2_scalar_forecast",
+#                           "ranger_fun", FALSE, "ranger_2_scalar_forecast")
+
+
+
+
+cv_results = prepare_model_list(h_all = h_all, model_fun_tibble = model_fun_tibble, dates_test = dates_test, 
+                                window_type = window_type, series_data = rus_m_full_stable,
+                                dependent = "value")
+cv_results_new = estimate_and_forecast(cv_results)
+
+mae_table = calculate_mae_table(cv_results_new)
+
+mae_table %>% tail()
+write_csv(mae_table, "estimation_results/mae_table_ind_prod.csv")
+
+
+# real forecasting....
+
+
+
+# models in tibble version ------------------------------------------------
+
+the_forecasts = prepare_model_list2(h_all = h_all, model_fun_tibble = model_fun_tibble, series_data = rus_m_full_stable)
+
+the_forecasts_new = estimate_and_forecast(the_forecasts)
+
+only_numbers = select(the_forecasts_new, date, h, model_fun, point_forecast)
+write_csv(only_numbers, path = "estimation_results/forecasts_ind_prod.csv")
 
 
 
