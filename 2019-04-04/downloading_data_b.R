66d402f0a0bbbe8986d687d14d135ee1b63b2841
Boris Demeshev <boris.demeshev@gmail.com>
2019-04-04 10:30:40 +0300

    better downloading_data

--- 2019-04-04/downloading_data_b.R
+++ 2019-04-04/downloading_data_b.R
@@ -1,30 +1,32 @@
+info = Sys.info() # получаем информацию о системе
+
+if (info[1] == "Linux") {
+  set_libreoffice_path("/usr/bin/libreoffice")  # ubuntu or macos
+  Sys.setenv(LD_LIBRARY_PATH = "/usr/lib/libreoffice/program/") # ubuntu protection against libreglo.so not found
+}
+
+if (info[1] == "Windows") {
+  Sys.setenv("TAR" = "internal") # if install_github() fails on Windows OS
+  set_libreoffice_path("C:/Program Files/LibreOffice/program/soffice.exe")  # windows
+}
+
+path = "~/Documents/kassandra/data/raw/"
+path = "D:/Research/Kassandra/data/raw/"
+
+
 # devtools::install_github("kassandra-ru/kassandr")
-# Sys.setenv("TAR" = "internal") # if install_github() fails on Windows OS
-
-
 library(kassandr)
+
 library(tidyverse)
 library(rio)
 library(lubridate)
 library(docxtractr)
 
-set_libreoffice_path("/usr/bin/libreoffice")  # ubuntu or macos
-set_libreoffice_path("C:/Program Files/LibreOffice/program/soffice.exe")  # windows
-
-Sys.setenv(LD_LIBRARY_PATH = "/usr/lib/libreoffice/program/") # ubuntu protection against libreglo.so not found
-
-path = "~/Documents/kassandra/data/raw/"
-path = "D:/Research/Kassandra/data/raw/"
 
 path_day = paste0(path, "/", Sys.Date(), "/") # add current date to path
 
-watchdog_file = paste0(path, "watchdog.csv")
+watchdog_file = paste0(path, "watchdog.csv") 
 watchdog = import(watchdog_file)
-
-# this was done to setup download log file for the first time
-# download_log_first = watchdog %>% mutate(access_date = Sys.Date(), access_status = NA, hash_raw = NA, hash_main = NA) %>% head(0)
-# export(download_log_first, download_log_file)
-
 
 # download all
 download_log_new = download_statistics(path, watchdog)
